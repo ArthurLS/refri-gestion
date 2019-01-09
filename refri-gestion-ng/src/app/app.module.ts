f0bc7157a18786b33da006f0c51b4e3d4e78a400
ArthurLS <arthur.lesaint@hotmail.fr>
2019-01-09 15:56:57 +0100

    All pages and components added with a fast menu

--- refri-gestion-ng/src/app/app.module.ts
+++ refri-gestion-ng/src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ScannerComponent } from './scanner/scanner.component';
+import { FridgeComponent } from './fridge/fridge.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScannerComponent,
+    FridgeComponent,
+    ShopListComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
